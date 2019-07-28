9420c382b0c42dae9f00880fd22801c8f5a2943a
Joey ter Meulen <joeytermeulen@gmail.com>
2019-07-28 20:00:54 +0200

    Player can throw ball

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,33 +5,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Customizable variables
+    // Configuration variables
     [SerializeField] float playerSpeed = 10f;
     [SerializeField] float gameSpacePadding = 1f;
+    [SerializeField] GameObject ballPrefab;
+    [SerializeField] float ballSpeed = 20f;
+    [SerializeField] float ballCooldown = .3f;
 
     // Variable init
     float minX;
     float maxX;
     float minY;
     float maxY;
+    Coroutine throwBallCoroutine;
 
     private void Start()
     {
-        SetUpWorldBoundaries();
-    }
+        if(!ballPrefab)
+        {
+            Debug.LogError("Could not find ball prefab on player");
+        }
 
-    private void SetUpWorldBoundaries()
-    {
-        Camera mainCamera = Camera.main;
-        minX = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + gameSpacePadding;
-        maxX = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - gameSpacePadding;
-        minY = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + gameSpacePadding;
-        maxY = mainCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - gameSpacePadding;
+        SetUpWorldBoundaries();
     }
 
     private void Update()
     {
         Move();
+        ThrowBall();
     }
 
     private void Move()
@@ -43,5 +44,38 @@ public class Player : MonoBehaviour
         float newYPos = Mathf.Clamp(transform.position.y + deltaY, minY, maxY);
 
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void ThrowBall()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            throwBallCoroutine = StartCoroutine(ThrowBallCoroutine());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(throwBallCoroutine);
+        }
+    }
+
+    IEnumerator ThrowBallCoroutine()
+    {
+        while(true)
+        {
+            GameObject ball = Instantiate(
+                ballPrefab, transform.position,
+                ballPrefab.transform.rotation) as GameObject;
+            ball.GetComponent<Rigidbody2D>().velocity = new Vector2(0, ballSpeed);
+            yield return new WaitForSeconds(ballCooldown);
+        }
+    }
+
+    private void SetUpWorldBoundaries()
+    {
+        Camera mainCamera = Camera.main;
+        minX = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + gameSpacePadding;
+        maxX = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - gameSpacePadding;
+        minY = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + gameSpacePadding;
+        maxY = mainCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - gameSpacePadding;
     }
 }
