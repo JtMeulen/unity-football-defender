a6c55c0dad008f592d6b22d2dd56c68a2dcf25ff
Joey ter Meulen <joeytermeulen@gmail.com>
2019-07-30 23:17:53 +0200

    setup for enemy waves and paths

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    // Variable init
+    [SerializeField] WaveConfig waveConfig;
+
+    private List<Transform> waypoints;
+    private int currentWaypoint = 0;
+    private float enemySpeed = 4f;
+
+    private void Start()
+    {
+        // Get all data from config file
+        waypoints = waveConfig.GetWaypoints();
+
+        transform.position = waypoints[currentWaypoint].transform.position;
+
+    }
+
+    private void Update()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        // Keep enemy moving to last waypoint, if reached, destroy
+        if (currentWaypoint < waypoints.Count)
+        {
+            // Get currentWaypoint position
+            var targetPosition = waypoints[currentWaypoint].transform.position;
+            var movementSpeed = enemySpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementSpeed);
+
+            // Check if enemy reached waypoint to go to next waypoint
+            if (transform.position == targetPosition)
+            {
+                currentWaypoint++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+
+    private int startingWaveIndex = 0;
+
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWaveIndex];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig currentWave)
+    {
+        for(int i = 0; i < currentWave.GetTotalEnemies(); i++)
+        {
+            Instantiate(
+                currentWave.GetEnemyPrefab(),
+                currentWave.GetWaypoints()[0].transform.position,
+                Quaternion.identity);
+            yield return new WaitForSeconds(currentWave.GetTimeBetweenSpawns());
+        }
+    }
+}
